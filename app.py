3a7d8781a23f6c195d29bc9ea27df3a852948646
Abdullah Kazi <81045582+Abdullah-Kazi@users.noreply.github.com>
2024-06-05 23:14:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,46 +11,11 @@ import plotly.graph_objects as go
 import tarfile
 import urllib.request
 
-
-MODEL_URL = "https://github.com/Abdullah-Kazi/nlp-pipeline/raw/1d34f53ef652286efa7e563a47c30825db20b745/en_core_web_sm-3.7.1.tar.gz"
-MODEL_PATH = "en_core_web_sm-3.7.1.tar.gz"
-EXTRACT_PATH = "./models"
-
-@st.cache(allow_output_mutation=True, show_spinner=True)
-def download_and_setup_spacy_model():
-    # Ensure the download directory exists
-    if not os.path.exists(EXTRACT_PATH):
-        os.makedirs(EXTRACT_PATH)
-
-    # Download the model tar file if not present
-    if not os.path.exists(os.path.join(EXTRACT_PATH, MODEL_PATH)):
-        urllib.request.urlretrieve(MODEL_URL, os.path.join(EXTRACT_PATH, MODEL_PATH))
-
-    # Define the model directory inside the extract path
-    model_directory = os.path.join(EXTRACT_PATH, "en_core_web_sm-3.7.1")
-
-    # Extract the model if the directory doesn't exist
-    if not os.path.exists(model_directory):
-        with tarfile.open(os.path.join(EXTRACT_PATH, MODEL_PATH), "r:gz") as tar:
-            tar.extractall(path=EXTRACT_PATH)
-
-    # Check if the config file exists
-    if not os.path.exists(os.path.join(model_directory, "config.cfg")):
-        st.error("Model configuration file not found after extraction.")
-        st.stop()
-
-    # Load the model from the extracted path
-    try:
-        nlp_model = spacy.load(model_directory)
-    except Exception as e:
-        st.error(f"Failed to load spaCy model from {model_directory}: {e}")
-        st.stop()
-
-    return nlp_model
-
-# Load the model
-nlp = download_and_setup_spacy_model()
-
+@st.cache_resource
+def load_model():
+    nlp = spacy.load("en_core_web_sm-3.7.1")
+    return nlp
+nlp = load_model()
 
 def download_nltk_resources():
     nltk.download('stopwords')
