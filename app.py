985085a6a8781c9e47b8993de592a309eec2cf54
Abdullah Kazi <81045582+Abdullah-Kazi@users.noreply.github.com>
2024-06-05 23:15:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import urllib.request
 
 @st.cache_resource
 def load_model():
-    nlp = spacy.load("en_core_web_sm-3.7.1")
+    nlp = spacy.load("en_core_web_sm")
     return nlp
 nlp = load_model()
 
